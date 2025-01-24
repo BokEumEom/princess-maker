60458ee9243044dc23fd9093168c48a647454ac7
bokeum <bokmail83@gmail.com>
2025-01-24 18:02:38 +0900

    UI 업데이트트

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,47 +1,43 @@
-// src/components/QuestionCard.jsx
-import Avatar from './Avatar';
+/* QuestionCard.jsx */
+import React from "react";
+import Avatar from "./Avatar";
 
 const QuestionCard = ({ question, onSelect, triggerRandomEvent, avatar }) => {
   const handleClick = (effect, option) => {
     onSelect(effect, option);
-
-    // 랜덤 이벤트 트리거
     if (option.triggerEvent) {
       triggerRandomEvent();
     }
   };
 
   return (
-    <div className="question-card">
-      {/* 질문 이미지와 아바타 오버레이 */}
+    <div className="full-screen-card">
+      {/* 배경 이미지 */}
       {question.image && (
-        <div className="question-image-wrapper">
-          <img
-            src={question.image}
-            alt="질문 이미지"
-            className="question-image"
-          />
-          {/* 아바타 오버레이 */}
-          <div className="avatar-overlay">
-            <Avatar avatar={avatar} />
-          </div>
+        <div className="background-image">
+          <img src={question.image} alt="질문 이미지" />
         </div>
       )}
 
-      {/* 질문 텍스트 */}
-      <h3 className="question-text">{question.question}</h3>
+      {/* 아바타 오버레이 */}
+      <div className="avatar-overlay">
+        <Avatar avatar={avatar} />
+      </div>
 
-      {/* 선택지 버튼 */}
-      <div className="options">
-        {question.options.map((option, index) => (
-          <button
-            key={index}
-            className="option-btn"
-            onClick={() => handleClick(option.effect, option)}
-          >
-            {option.text}
-          </button>
-        ))}
+      {/* 질문 및 선택지 */}
+      <div className="overlay-content">
+        <h3 className="question-text">{question.question}</h3>
+        <div className="options">
+          {question.options.map((option, index) => (
+            <button
+              key={index}
+              className="option-btn"
+              onClick={() => handleClick(option.effect, option)}
+            >
+              {option.text}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
